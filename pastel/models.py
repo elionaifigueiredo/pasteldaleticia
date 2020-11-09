873b2a93a409fa34fd67605bce5efc5c3cbb6bea
Elionai Figueiredo <elionaifigueiredo@gmail.com>
2020-11-08 21:57:30 -0400

    versao2

--- pastel/models.py
+++ pastel/models.py
@@ -6,6 +6,6 @@ from django.db import models
 
 class Pastel(models.Model):
     nome = models.CharField(max_length=200)
-    
+      
     def __str__(self):
         return self.nome
--- pastel/views.py
+++ pastel/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from django.utils import timezone
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'pastel/index.html', {})    
+    msg = "Ola Mundo Django"
+    return render(request, 'pastel/index.html', {'msg': msg})    
