8f92e2e6a45dc79b49bbd93ae2d1081f2df4c0b9
Elionai Figueiredo <elionaifigueiredo@gmail.com>
2020-11-08 22:51:12 -0400

    versao3

--- pastel/views.py
+++ pastel/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.http import HttpResponse
 
+from .models import Pastel
+
 # Create your views here.
 
 def index(request):
     msg = "Ola Mundo Django"
-    return render(request, 'pastel/index.html', {'msg': msg})    
+    lista = Pastel.objects.all()
+    return render(request, 'pastel/index.html', {'msg': msg, 'lista':lista})    
