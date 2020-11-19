08259b88eb2cce90d3f145aef98f4f03f2e5eca9
Elionai Figueiredo <elionaifigueiredo@gmail.com>
2020-11-18 20:56:02 -0400

    versao5

--- pastelaria/settings.py
+++ pastelaria/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'e6(*x)=_h&d^wag=rb9^nb=&%934!k-lv^b9=8cuwa1=oq@-+b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
